3379ad6beae3ea55be60dc83cdf617e4c67dcda5
Nirajan09 <nirajantiwari09@gmail.com>
2025-07-18 16:06:59 +0545

    added interactive container

--- src/components/trusted-companies/index.jsx
+++ src/components/trusted-companies/index.jsx
@@ -8,22 +8,28 @@ const TrustedCompaniesSection = () => {
             </p>
             <div className="w-full grid grid-cols-3 lg:grid-cols-6 gap-y-8 gap-x-6 items-center justify-items-center">
                 <div>
-                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/65c4cdd76ec1f96fd1e9ff8b_logo-nokia.svg" alt="" />
+                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/65c4cdd76ec1f96fd1e9ff8b_logo-nokia.svg" className='className="transition duration-200 hover:shadow-lg hover:scale-105 p-2 cursor-pointer object-contain
+                    "' alt="" />
                 </div>
                 <div>
-                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c186489234871b0050d8d1_logo-bumble.svg" alt="" />
+                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c186489234871b0050d8d1_logo-bumble.svg" className='className="transition duration-200 hover:shadow-lg hover:scale-105 p-2 cursor-pointer object-contain
+                    "' alt="" />
                 </div>
                 <div>
-                    <img loading="lazy" src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c18648d1201ba13fea32fe_logo-schibsted.svg" alt="" />
+                    <img loading="lazy" src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c18648d1201ba13fea32fe_logo-schibsted.svg" className='className="transition duration-200 hover:shadow-lg hover:scale-105 p-2 cursor-pointer object-contain
+                    "' alt="" />
                 </div>
                 <div>
-                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/65c4cdd71a615f6f78fabaf4_logo-opera.svg" alt="" />
+                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/65c4cdd71a615f6f78fabaf4_logo-opera.svg" className='className="transition duration-200 hover:shadow-lg hover:scale-105 p-2 cursor-pointer object-contain
+                    "' alt="" />
                 </div>
                 <div>
-                    <img loading="lazy" src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c18648d559059598d91f6b_logo-bonterra.svg" alt="" />
+                    <img loading="lazy" src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c18648d559059598d91f6b_logo-bonterra.svg" className='className="transition duration-200 hover:shadow-lg hover:scale-105 p-2 cursor-pointer object-contain
+                    "' alt="" />
                 </div>
                 <div>
-                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c18648f7fac6ef7ff45ea9_logo-zoom.svg" alt="" />
+                    <img src="https://cdn.prod.website-files.com/63f38a8c92397a024fcb9ae8/67c18648f7fac6ef7ff45ea9_logo-zoom.svg" className='className="transition duration-200 hover:shadow-lg hover:scale-105 p-2 cursor-pointer object-contain
+                    "' alt="" />
                 </div>
             </div>
         </div>
