2c702cbd0663e85c58639ee2e8cd58932bad65e1
Nirajan09 <nirajantiwari09@gmail.com>
2025-07-23 14:39:02 +0545

    Optimization of the container

--- src/components/founder-section/index.jsx
+++ src/components/founder-section/index.jsx
@@ -3,7 +3,7 @@ import React, { lazy, Suspense } from "react";
 const FounderImage = lazy(() => import("../lazyloading-utils/founder-section/FounderSection"));
 
 const FounderSection = () => (
-  <div className="section max-h-[350px] flex flex-col md:flex-row-reverse items-start md:items-stretch justify-between rounded-xl mx-auto">
+  <section className="section max-h-[350px] flex flex-col md:flex-row-reverse items-start md:items-stretch justify-between rounded-xl mx-auto">
     {/* Text Section */}
     <div className="md:w-[60%]">
       <p className="text-white text-justify text-2xl lg:text-4xl font-medium leading-snug">
@@ -12,20 +12,17 @@ const FounderSection = () => (
       </p>
     </div>
     {/* Image + Caption Section */}
-    <div className="flex  gap-4 mt-3 items-center md:items-start md:w-1/4 md:flex-col">
+    <div className="flex gap-4 mt-3 items-center md:items-start md:w-1/4 md:flex-col">
       <div className="mb-2">
-        <Suspense
-          fallback={<div className="w-[158px] h-[64px] bg-gray-700 rounded-lg animate-pulse" />}
-        >
+        <Suspense fallback={<div className="w-[158px] h-[64px] bg-gray-700 rounded-lg animate-pulse" />}>
           <FounderImage className="rounded-lg shadow" />
         </Suspense>
       </div>
-      <div className="text-xs text-gray-300 text-center md:text-left mt-2 sm:text-size-xl hover:underline transition-all cursor-pointer">
-  FOUNDERS<br />OF HALO LAB
-</div>
-
+      <div className="text-xs text-gray-300 text-center md:text-left mt-2 sm:text-xl hover:underline transition-all cursor-pointer">
+        FOUNDERS<br />OF HALO LAB
+      </div>
     </div>
-  </div>
+  </section>
 );
 
 export default FounderSection;
